3bb816d32eec9e6164dbe92a6f5271775bcca72b
lukiod <mohakgupta0981@gmail.com>
2025-12-08 13:33:36 +0530

    test1 fixing window nvcc files

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -7,15 +7,27 @@ fn test_data_model_integration() {
     // Test GPU info
     let gpu = GpuInfo::new("RTX 3080".to_string(), GpuVendor::Nvidia);
     assert!(matches!(gpu.vendor, GpuVendor::Nvidia));
+    // let installation = CudaInstallation::new(
+    //     "11.8".to_string(),
+    //     PathBuf::from("/usr/local/cuda-11.8")
+    // );
     
     // Test CUDA installation
+    let install_path = if cfg!(windows) {
+        PathBuf::from("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.8")
+    } else {
+        PathBuf::from("/usr/local/cuda-11.8")
+    };
     let installation = CudaInstallation::new(
         "11.8".to_string(),
-        PathBuf::from("/usr/local/cuda-11.8")
+        install_path.clone()
     );
     assert_eq!(installation.version, "11.8");
-    assert_eq!(installation.get_nvcc_path(), PathBuf::from("/usr/local/cuda-11.8/bin/nvcc"));
+    // assert_eq!(installation.get_nvcc_path(), PathBuf::from("/usr/local/cuda-11.8/bin/nvcc"));
+    let binary_name = if cfg!(windows) { "nvcc.exe" } else { "nvcc" }
+    let expected_path = install_path.join("bin").join(binary_name);
     
+    assert_eq!(installation.get_nvcc_path(), expected_path);
     // Test config
     let config = CudaMgrConfig::default();
     assert!(config.auto_cleanup);
